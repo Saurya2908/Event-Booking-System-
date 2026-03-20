d9f2c9c5b3e0e8bb69567cba6625ee2254ef6f94
Shaurya <shaurya29.98@gmail.com>
2026-03-20 19:50:28 +0530

    added railway db config

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root123",
   database: process.env.DB_NAME || "event_booking",
+  port: process.env.DB_PORT || 3306
 });
 
 module.exports = pool;
